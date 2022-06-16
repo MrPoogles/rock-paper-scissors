39135e20a526df19023da35647798179db985362
Fauzan <fauzan.hanif.alfarisi@gmail.com>
2022-06-16 22:56:23 +0900

    Add function playerPlay for player to choose 'rock', 'paper', or 'scissors'

--- javascript.js
+++ javascript.js
@@ -45,9 +45,23 @@ function playRound(playerSelection, computerSelection) {
       } else {
         return "You Win! Scissors cut paper.";
       }
+  } else {
+    return "You did not choose. Your indecisiveness has cost you a win."
   }
 }
 
-const playerSelection = "paper";
+// function to input player choice via promptbox
+function playerPlay() {
+  let playerChoice = prompt("Please select between 'rock', 'paper', or 'scissors'");
+  if (playerChoice.toLowerCase() == "rock") {
+    return "rock";
+  } else if (playerChoice.toLowerCase() == "paper") {
+    return "paper";
+  } else if (playerChoice.toLowerCase() == "scissors") {
+    return "scissors";
+  }
+}
+
+const playerSelection = playerPlay();
 const computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection));
