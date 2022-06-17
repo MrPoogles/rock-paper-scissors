36052c83df340868887eda9901488896a281c702
Fauzan <fauzan.hanif.alfarisi@gmail.com>
2022-06-17 22:06:28 +0900

    Delete comment and tidy up for cleaner view

--- javascript.js
+++ javascript.js
@@ -1,9 +1,3 @@
-// Janken! is a game between user and computer.
-// Program doen not need to have interface. It's console-based. 
-// User will input data by choosing one of three choices, input from a prompt.
-// It is desired that the computer, will also pick one among 3 choices randomly.
-// Output will be determined by the result of user and computer choices. 
-
 // Computer will pick randomly 1 of 3 numbers.
 function computerPlay() {
   let choice = Math.floor(Math.random() * 3);
@@ -19,35 +13,32 @@ function computerPlay() {
 }
 
 //function that plays a single round of Janken!
-//player choice = comp choice -> tie
 function playRound(playerSelection, computerSelection) {
   if (playerSelection == computerSelection) {
-    return "It's a tie!";
+    console.log("It's a tie! You both choose the same option.");
    
   // rock
   } else if (playerSelection == "rock") {
       if (computerSelection == "paper") {
-        return "You Lose! Paper beats rock.";
+        console.log("You Lose! Rock cannot beat paper.");
       } else {
-        return "You Win! Rock slams scissors!";
+        console.log("You Win! Rock slams scissors!");
       }
   // paper
   } else if (playerSelection == "paper") {
       if (computerSelection == "scissors") {
-        return "You Lose! Scissors cut paper.";
+        console.log("You Lose! Paper was cut by scissors.");
       } else {
-        return "You Win! Paper beats rock.";
+        console.log("You Win! Paper beats rock.");
       }
   // scissors
   } else if (playerSelection == "scissors") {
       if (computerSelection == "rock") {
-        return "You Lose! Rock slams scissors.";
+        console.log("You Lose! Scissors was slammed by rock.");
       } else {
-        return "You Win! Scissors cut paper.";
+        console.log("You Win! Scissors cut paper.");
       }
-  } else {
-    return "You did not choose. Your indecisiveness has cost you a win."
-  }
+  } 
 }
 
 // function to input player choice via promptbox
@@ -59,9 +50,18 @@ function playerPlay() {
     return "paper";
   } else if (playerChoice.toLowerCase() == "scissors") {
     return "scissors";
+  } else {
+    console.log("Please choose between 'rock, 'paper' or scissors'!");
   }
 }
 
-const playerSelection = playerPlay();
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+
+let playerSelection = playerPlay();
+let computerSelection = computerPlay();
+
+// function to play 5 round game
+function game() {
+  playRound(playerSelection, computerSelection);
+}
+
+game();
